f8275726b9b0d26300ea70ccdfa24ebe1ab14c5b
李杰 <32835610+jedi007@users.noreply.github.com>
2021-08-19 14:40:42 +0800

    加解密工具

---
+++ decryptLD.py
@@ -0,0 +1,13 @@
+import os
+
+def crypt(addtxt,basedir):
+    for root,dirs,files in os.walk(basedir): 
+        for file in files: 
+            name_o = os.path.join(root,file).replace("\\","/")
+            if addtxt:
+                os.rename(name_o,name_o +".txt")
+            else:
+                os.rename(name_o,name_o[0:-4])
+
+if __name__ == '__main__':
+    crypt(0,"D:/TestData/testdecrypt")
